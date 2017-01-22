613b0197a1e7b89ea62abffb08a7a454a73164e7
mustafin <mustafin.sa@gmail.com>
2017-01-22 22:09:42 +0300

    some refactoring

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
 var thinFilmApp = angular.module('thinFilmApp', []);
 
-thinFilmApp.controller('mainController', function ($scope) {
-
-});
-
-thinFilmApp.controller('gridController', function ($scope) {
+function gridController($scope, thinFilmService) {
 
     $scope.editedRowIndex = -1;
     $scope.layers = [new Layer('', 1.2, 0.4)];
+    $scope.lambda0 = 550;
+    $scope.lambda = 550;
 
     $scope.addRow = function () {
         $scope.layers.push(new Layer());
@@ -21,39 +19,47 @@ thinFilmApp.controller('gridController', function ($scope) {
     };
 
     $scope.calculate = function () {
+        $scope.result = thinFilmService.calculate($scope.layers, $scope.lambda0, $scope.lambda);
+        $scope.resultIsReady = true;
+    };
+}
 
+thinFilmApp.controller('gridController', gridController);
+thinFilmApp.service('thinFilmService', thinFilmService);
+
+function thinFilmService() {
+    this.calculate = function (layers, lambda0, lambda) {
         var N_IN = 1;
         var N_OUT = 1.5;
         var i1 = math.complex(0, 1);
         var matrix = null;
 
-        for (var i = 0; i < $scope.layers.length; i++) {
-            var layer = $scope.layers[i];
-            var refractiveIndex = +layer.refraction;
-            var thickness = +layer.thickness;
+        for (var i = 0; i < layers.length; i++) {
+            var layer = layers[i];
+            var refractiveIndex = +(layer.refraction);
+            var thickness = +(layer.thickness);
 
-            var lambda = thickness; //TODO use real n*d
+            var d = thickness * lambda0 / refractiveIndex;
 
-            var phi0 = 2 * Math.PI * thickness / lambda;
+            var phi0 = 2 * Math.PI * refractiveIndex * d / lambda;
             var cosPhi0 = Math.cos(phi0);
             var sinPhi0 = Math.sin(phi0);
-            console.log(cosPhi0);
 
-            var m = [[0, math.complex(0, 1 / refractiveIndex)], [math.complex(0, refractiveIndex), 0]];
+            var m = [[cosPhi0, math.complex(0, sinPhi0 / refractiveIndex)], [math.complex(0, sinPhi0 * refractiveIndex), cosPhi0]];
             matrix = matrix ? math.multiply(matrix, m) : m;
         }
 
-        var r0 = math.add(math.multiply(N_IN, matrix[0][0]), math.multiply(i1, refractiveIndex, matrix[0][1]));
+        var r0 = math.add(math.multiply(N_IN, matrix[0][0]), math.multiply(i1, N_OUT, matrix[0][1]));
         var r1 = math.add(math.multiply(N_OUT, matrix[1][1]), math.multiply(i1, matrix[1][0]));
 
         var rMinus = math.add(r0, math.multiply(-1, r1));
         var rSum = math.add(r0, r1);
 
-        $scope.result = (Math.pow(rMinus.re, 2) + Math.pow(rMinus.im, 2)) / (Math.pow(rSum.re, 2) + Math.pow(rSum.im, 2));
-        $scope.resultIsReady = true;
+        var numerator = Math.pow(rMinus.re, 2) + Math.pow(rMinus.im, 2);
+        var denominator = Math.pow(rSum.re, 2) + Math.pow(rSum.im, 2);
+        return numerator / denominator;
     }
-
-});
+}
 
 function Layer(material, refraction, thickness) {
     this.material = material || 'Air';
---
+++ readme.md
@@ -0,0 +1,4 @@
+App for calculating performance of thin film systems.
+
+Deployed app:
+https://thin-film-calc.herokuapp.com/
